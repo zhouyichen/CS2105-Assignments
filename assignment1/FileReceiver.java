37299bc08389493e34405274f1b61f0ac600f6fa
zhouyichen <zhouyichen123@gmail.com>
2016-02-05 23:07:19 +0800

    [A1] Implement file receiver

--- assignment1/FileReceiver.java
+++ assignment1/FileReceiver.java
@@ -1,5 +1,5 @@
-// <Fill in your name> 
-
+// <Zhou Yichen> 
+import java.io.*;
 import java.net.*;
 
 class FileReceiver {
@@ -19,6 +19,37 @@ class FileReceiver {
     }
     
     public FileReceiver(String localPort) {
-        
+        try {
+            int portNumber = Integer.parseInt(localPort);
+            this.socket = new DatagramSocket(portNumber);
+
+            byte[] buffer = new byte[1000];
+
+            // get file name
+            this.pkt = new DatagramPacket(buffer, buffer.length);
+            this.socket.receive(this.pkt);
+            String fileName = new String(this.pkt.getData(), 0, this.pkt.getLength());
+
+            // preapre file output
+            FileOutputStream fos = new FileOutputStream(fileName);
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+            while (true) {
+                // receive every packet and write them into the file
+                this.pkt = new DatagramPacket(buffer, buffer.length);
+                this.socket.receive(this.pkt);
+                bos.write(this.pkt.getData(), 0, this.pkt.getLength());
+
+                // if the length of the packet is 0, this is the end of the file
+                if (this.pkt.getLength() == 0) {
+                    break;
+                }
+            }
+            bos.close();
+            this.socket.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
